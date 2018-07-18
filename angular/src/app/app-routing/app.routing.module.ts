e6675fe95125177f209a832942d5d9c9251b0d62
Loise <lobo.loise@gmail.com>
2018-07-18 03:27:28 -0300

    # consertando bug de redundancia

--- angular/src/app/app-routing/app.routing.module.ts
+++ angular/src/app/app-routing/app.routing.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
-import { AppRoutingModule } from './app-routing/app.routing.module';
 
 import { CommonModule } from '@angular/common';
 
