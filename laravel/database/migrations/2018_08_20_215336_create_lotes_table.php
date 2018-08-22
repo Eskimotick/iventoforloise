b91527b12ec591ca95594c6a1f6acdcc6d413dfa
Carrasco <fernando.carrasco0@gmail.com>
2018-08-21 22:02:45 -0300

    + Criadas as migrations de Configurações, Lotes(ainda sem chave estrangeira) e Pacotes.

---
+++ laravel/database/migrations/2018_08_20_215336_create_lotes_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLotesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('lotes', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('lote')->unsigned();
+            $table->integer('pacote_id')->unsigned();
+            $table->integer('vagas')->unsigned();
+            $table->integer('vagas_ocupadas')->unsigned()->default(0);
+            $table->string('descricao')->nullable();
+            $table->string('valor')->nullable();
+            $table->string('vencimento');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('lotes');
+    }
+}
