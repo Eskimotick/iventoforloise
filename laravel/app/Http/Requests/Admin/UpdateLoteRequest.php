0291ce9b6762a9eefd55f0aff9603210b9eb649b
Carrasco <fernando.carrasco0@gmail.com>
2018-08-22 21:52:28 -0300

    +Criada as validações de update dos Lotes.

---
+++ laravel/app/Http/Requests/Admin/UpdateLoteRequest.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Requests\Admin;
+
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
+
+class UpdateLoteRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    protected function failedValidation(Validator $validator)
+    {
+      throw new HttpResponseException(response()->json($validator->errors(), 400));
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'descricao' => 'string|min:10|max:300',
+            'vagas' => 'integer|min:1',
+            'valor' => 'string|regex:/[0-9]+\.[0-9]{2}/',
+            'vencimento' => 'string|regex:/[0-9]{4}-[0-9]{2}-[0-9]{2}\s{1}[0-2]{1}[0-9]{1}:[0-5]{1}[0-9]{1}:[0-5]{1}[0-9]{1}/' ,
+        ];
+    }
+
+    public function messages(){
+        return [
+            'descricao.min' => 'A descrição deve ter no mínimo 10 caracteres.',
+            'descricao.max' => 'A descrição deve ter no máximo 300 caracteres',
+            'vagas.integer' => 'O número de vagas deve ser um número inteiro.',
+            'vagas.min' => 'O número de vagas deve ser maior que zero.',
+            'valor.regex' => 'O valor deve obedecer o seguinte formato: 10.00',
+            'vencimento.regex' => 'O vencimento deve obedecer o seguinte formato: 2018-12-24 23:59:59',
+        ];
+    }
+}
