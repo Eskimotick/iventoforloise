208b71698947c8708243846e8614b862f9b65cb4
Wesley Mota <wesleymota@ejcm.com.br>
2018-08-23 14:39:02 -0300

    * Consertado problema do regex :)'

--- laravel/app/Http/Requests/Admin/UpdateLoteRequest.php
+++ laravel/app/Http/Requests/Admin/UpdateLoteRequest.php
@@ -33,8 +33,8 @@ class UpdateLoteRequest extends FormRequest
         return [
             'descricao' => 'string|min:10|max:300',
             'vagas' => 'integer|min:1',
-            'valor' => 'string|regex:/[0-9]+\.[0-9]{2}/', //10.60
-            'vencimento' => 'string|regex:/[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/' ,
+            'valor' => array('regex:/^[1-9][0-9]+\.[0-9]{2}$/'), 
+            'vencimento' => array('regex:/^[0-9]{4}-(0[1-9]|1[0-2])-(0[1-9]|[1-2][0-9]|3[0-1])\s([0-1][0-9]|2[0-3]):[0-5][0-9]:[0-5][0-9]$/') ,
         ];
     }
 
