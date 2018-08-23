49ccc24fd5adefa96940161859983ad98b823a64
Wesley Mota <wesleymota@ejcm.com.br>
2018-08-23 14:41:09 -0300

    + Terminada verificação das vagas do update. + Feita verificação dos vencimentos.

--- laravel/app/Models/Admin/Lote.php
+++ laravel/app/Models/Admin/Lote.php
@@ -25,15 +25,20 @@ class Lote extends Model
 
     //método utilizado pelo admin
     public function updateLote($request){
-
+        
+        //pega quantidade de lotes do Pacote
+        $lotesPacote = $this->belongsTo('App\Models\Admin\Pacote', 'pacote_id')->max('lotes');
+        
         if($request->vagas){
-
+            
+            //faz as validações das quantidades de vagas entre os lotes
             $resposta = $this->verificaVagas($request->vagas);
+
             if(gettype($resposta) == 'string'){
                 return $resposta;
             } 
             else{
-                $this->vagas = $request->vagas;
+                $this->vagas = $resposta;
             }
             
         }
@@ -44,8 +49,22 @@ class Lote extends Model
             $this->valor = $request->valor;
         }
         if($request->vencimento){
-            $this->vencimento = $request->vencimento;
+            $hoje = date('Y-m-d');
+            $vencimento = explode(' ',$request->vencimento, 2)[0];            
+
+            if($hoje >= $vencimento){
+                return 'A data de vencimento deve ser uma data futura.';
+            }
+            else{
+                $this->vencimento = $request->vencimento;
+            }
+            
         }
+        
+        //se for ultimo lote o vencimento é a data final das inscrições
+        // if($this->lote == $lotesPacote){
+        //     $this->vencimento = $config->fim_inscricoes;
+        // }
 
         $this->save();
         
@@ -54,25 +73,55 @@ class Lote extends Model
     public function verificaVagas($vagas){
 
         //pega o pacote pai do lote
-        $pacote = $this->belongsTo('App\Models\Admin\Pacote', 'id')->first();
+        $pacote = $this->belongsTo('App\Models\Admin\Pacote', 'pacote_id')->first();
         
-        // se o número de vagas da request for maior que 
+        // se for o ultimo lote
+        if($this->lote == $pacote->lotes){
+            return 'Para alterar as vagas do último Lote, adicione ou remova vagas dos lotes anteriores.';
+        }
+        
+        //se o numero de vagas for igual ao das vagas atuais nao faz nada
+        if($vagas == $this->vagas){
+            return $this->vagas;
+        }
+        
+        // se o número de vagas da request for maior que as vagas do Pacote pai
         if($vagas >= $pacote->vagas){
             return 'Número de vagas deve ser menor que o número total de vagas do Pacote';
         }
 
+        // se o número de vagas da request for maior que as vagas do Lote 
         if($vagas > $this->vagas){
+            
             $ultimoLote = Lote::where('pacote_id', $pacote->id)->where('lote', $pacote->lotes)->first();
             $vagas = $vagas - $this->vagas;
             if($ultimoLote->vagas <= $vagas){
                 return 'O número de vagas disponíveis é menor que o número digitado';
             }
             else{
-                
+                $ultimoLote->vagas -= $vagas;
+                $ultimoLote->save();
+                $vagas += $this->vagas;
+                return $vagas;
             }
         }
 
+        // se o número de vagas da request for menor que as vagas do Lote
         if($vagas < $this->vagas){
+
+            $ultimoLote = Lote::where('pacote_id', $pacote->id)->where('lote', $pacote->lotes)->first();
+            $vagasRequest = 0;
+            $vagasRequest += $vagas;
+            
+            $vagas = $this->vagas - $vagas;
+            if($vagas < 0){
+                return 'As vagas não podem ser maior que o número de vagas do lote';
+            }
+            else{
+                $ultimoLote->vagas += $vagas;
+                $ultimoLote->save();    
+                return $vagasRequest;
+            }
 
         }
 
