2bebb85484d5c9f1cd9d5e1ad134ed4633a5a035
Carrasco <fernando.carrasco0@gmail.com>
2018-08-23 23:55:29 -0300

    + Criadas as funções de updateInscricoes, updateEvento e abrir e fechar as inscrições.

--- laravel/app/Http/Controllers/Admin/ConfigController.php
+++ laravel/app/Http/Controllers/Admin/ConfigController.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+
 use Illuminate\Http\Request;
+use App\Models\Admin\Config;
+use App\Http\Requests\Admin\UpdateConfigRequest as UpdateRequest;
 
 class ConfigController extends Controller
 {
@@ -45,9 +48,45 @@ class ConfigController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(Request $request, $id)
+    public function updateInscricoes(UpdateRequest $request)
     {
-        //
+        $config = Config::find(1);
+                
+        $resposta = $config->updateInscricoes($request);
+        if($resposta){
+            return response()->error($resposta, 400);    
+        }
+        
+        return response()->success($config);            
+        
+    }
+
+    public function updateEvento(UpdateRequest $request)
+    {
+        $config = Config::find(1);
+        
+        $resposta = $config->updateEvento($request);
+        if($resposta){
+            return response()->error($resposta, 400);    
+        }
+        
+        return response()->success($config);            
+        
+    }
+
+    public function abreInscricoes(){
+        $config = Config::find(1);
+        $config->adminChangeStatus(0);
+
+        return response()->success('Evento aberto com sucesso!');
+    }
+
+    public function fechaInscricoes(){
+        $config = Config::find(1);
+        $config->adminChangeStatus(2);
+
+        return response()->success('Evento fechado com sucesso!');
+        
     }
 
     /**
