04df7021c026fd0e41afa14b054512c54f6aea0c
Carrasco <fernando.carrasco0@gmail.com>
2018-08-23 23:57:11 -0300

    + Criada a validação do update das configurações da sprint. + Criadas as funções na model Config de atualizar as datas de inscrição do evento e as datas do evento. + Criada a função para mudar o status das inscrições(abrir/fechar).

--- laravel/app/Models/Admin/Config.php
+++ laravel/app/Models/Admin/Config.php
@@ -8,12 +8,78 @@ class Config extends Model
 {
 
     // metodo para setar datas das inscricoes
-    public function updateInscricoes(){
+    public function updateInscricoes($request){
 
+        if($request->inicio_inscricoes){
+            $hoje = date('Y-m-d');
+            $inicio_inscricoes = explode(' ',$request->inicio_inscricoes, 2)[0];            
+
+            if($hoje >= $inicio_inscricoes){
+                return 'A data de inicio das inscricoes deve ser uma data futura.';
+            }
+            else{
+                $this->inicio_inscricoes = $request->inicio_inscricoes;
+                $this->save();
+            }
+            
+        }
+
+        if($request->fim_inscricoes){
+
+            if($this->inicio_inscricoes >= $request->fim_inscricoes){
+                return 'A data de fim das inscrições deve ser após a data de inicio das inscrições.';
+            }
+            else{
+                $this->fim_inscricoes = $request->fim_inscricoes;
+                $this->save();
+            }
+            
+        }
+        
     }
 
     // método para setar datas do evento
-    public function updateEvento(){
+    public function updateEvento($request){
+        
+        if($request->inicio_evento){
+            // $hoje = date('Y-m-d');
+            // $inicio_evento = explode(' ',$request->inicio_evento, 2)[0];            
+            // if($hoje >= $inicio_evento){
+            //     return 'A data de inicio do evento deve ser uma data futura.';
+            // }
 
+            // $fim_inscricoes = explode(' ',$this->fim_inscricoes, 2)[0];
+            // if($fim_inscricoes >= $inicio_evento){
+                
+            if($this->fim_inscricoes >= $request->inicio_evento){
+                return 'A data de inicio do evento deve ser após o fim das inscrições.';
+            }
+            else{
+                $this->inicio_evento = $request->inicio_evento;
+                $this->save();
+            }
+            
+        }
+
+        if($request->fim_evento){          
+
+            if($this->inicio_evento >= $request->fim_evento){
+                return 'A data de fim do evento deve ser após a data de inicio do evento.';
+            }
+            else{
+                $this->fim_evento = $request->fim_evento;
+                $this->save();
+            }
+            
+        }
+    }
+
+    // metódo para trancar/abrir inscrições manualmente
+    public function adminChangeStatus($status){
+
+        // $status = 0 - evento aberto;
+        // $status = 2 - evento trancado pelo admin
+        $this->status = $status;
+        $this->save();
     }
 }
