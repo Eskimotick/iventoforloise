9fcdaf6160bca39373cc96466649af1b981120db
Carrasco <fernando.carrasco0@gmail.com>
2018-08-23 23:57:50 -0300

    * Só rearumando colunas para melhorar a leitura e entendimento da tabela.

--- laravel/database/migrations/2018_08_20_223125_create_configs_table.php
+++ laravel/database/migrations/2018_08_20_223125_create_configs_table.php
@@ -15,20 +15,20 @@ class CreateConfigsTable extends Migration
     {
         Schema::create('configs', function (Blueprint $table) {
             $table->increments('id');
+            $table->string('nome_evento');
             $table->integer('status');
                 // 0 - evento aberto;
                 // 1 - evento ainda não abriu
                 // 2 - evento trancado pelo admin
                 // 3 - inscriçoes fechadas
-            $table->string('inicio_evento');
-            $table->string('fim_evento');
             $table->string('inicio_inscricoes');
             $table->string('fim_inscricoes');
             $table->string('fim_pagamentos');
+            $table->string('inicio_evento');
+            $table->string('fim_evento');          
             $table->string('background_img_path')->nullable();
             $table->string('logo_img_path')->nullable();
             $table->string('favicon_img_path')->nullable();
-            $table->string('nome_evento');
             $table->timestamps();
         });
     }
