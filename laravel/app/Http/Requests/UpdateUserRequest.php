dc929555c79673224ba2b0de11fb2a9a6eb5e1e8
João Pedro <joaopedrodepaula@ejcm.com.br>
2018-08-26 03:01:47 -0300

    * Adicionando o atibuto unique na validação de email

--- laravel/app/Http/Requests/UpdateUserRequest.php
+++ laravel/app/Http/Requests/UpdateUserRequest.php
@@ -16,7 +16,7 @@ class UpdateUserRequest extends FormRequest
     {
       return [
         'name' => 'regex:/^[\pL\s\-]+$/u|max:255',
-        'email' => 'email|max:255',
+        'email' => 'email|unique:users|max:255',
         'password' => 'min:6',
         'c_password' => 'same:password',
       ];
@@ -26,15 +26,12 @@ class UpdateUserRequest extends FormRequest
     public function messages()
      {
        return [
-         'nickame.required' => 'É necessário fornecer um nome.',
          'nickame.regex' => 'O nome deve consistir apenas de caracteres alfabéticos.',
          'nickame.max' => 'O nome deve ter no máximo 255 caracteres.',
-         'email.required' => 'É necessário preencher o campo de e-mail.',
          'email.email' => 'O e-mail não está no formato correto',
+         'email.unique' => 'Este e-mail já foi cadastrado, entre com outro.',
          'email.max' => 'O e-mail deve ter no máximo 255 caracteres.',
-         'password.required' => 'É necessário entrar com uma senha.',
          'password.min' => 'A senha deve ter no mínimo 6 caracteres.',
-         'c_password.required' => 'Por favor confirme sua senha.',
          'c_password.same' => 'As senhas não batem.',
        ];
      }
