b711fd493757f7360f6e39fe11cb16f73afc730f
Carrasco <fernando.carrasco0@gmail.com>
2018-08-28 16:18:52 -0300

    + Cridas migrations de quartos e da tabela pivot pacotes_quartos.

---
+++ laravel/database/migrations/2018_08_28_103652_create_quartos_table.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateQuartosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('quartos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('hospedagem_id')->unsigned();
+            $table->string('nome');
+            $table->string('descricao');
+            $table->integer('vagas')->unsigned();
+            $table->integer('vagas_ocupadas')->unsigned()->default(0);
+            $table->integer('status')->default(0); 
+            $table->timestamps();
+
+            $table->foreign('hospedagem_id')
+                    ->references('id')->on('hospedagems')
+                    ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('quartos');
+    }
+}
