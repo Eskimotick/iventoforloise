438380a7986b8f7bb5637564f0363d2d9d513adf
João Pedro <joaopedrodepaula@ejcm.com.br>
2018-09-21 01:39:15 -0300

    + Rotas para inscrição e deleção de users em atividades. * Alterando padrões de comentários, colocando eles nas linhas de baixo das rotas em vez dos lados.

--- laravel/routes/api.php
+++ laravel/routes/api.php
@@ -14,19 +14,26 @@ use Illuminate\Http\Request;
 */
 
 // Visitantes podem ver a listagem de usuários.
-Route::get('user/{id}', 'UserController@show'); ////http://site.com/api/user/id
-Route::get('user', 'UserController@index'); //http://site.com/api/user
+Route::get('user/{id}', 'UserController@show');
+//http://site.com/api/user/id
+Route::get('user', 'UserController@index');
+//http://site.com/api/user
+
 // Rotas de log-in e cadastro para visitantes.
-Route::post('login', 'API\AuthController@login'); //http://site.com/api/login
-Route::post('register', 'API\AuthController@register'); //http://site.com/api/register
+Route::post('login', 'API\AuthController@login');
+//http://site.com/api/login
+Route::post('register', 'API\AuthController@register');
+//http://site.com/api/register
 
 // Grupo de rotas para password.
 Route::group([
     'prefix' => 'password'
 ], function (){
     // Rotas para recuperar / redefinir senha.
-    Route::post('/forgot', 'API\AuthController@forgotPassword'); //http://site.com/api/password/forgot
-    Route::post('/reset', 'API\AuthController@resetPassword'); //http://site.com/api/password/reset
+    Route::post('/forgot', 'API\AuthController@forgotPassword');
+    //http://site.com/api/password/forgot
+    Route::post('/reset', 'API\AuthController@resetPassword');
+    //http://site.com/api/password/reset
 });
 
 
@@ -35,13 +42,20 @@ Route::group([
 Route::group([
     'prefix' => 'mail'
 ], function (){
+
     // Rota para confirmação de email.
-    Route::post('confirm', 'API\AuthController@register')->name('confirm-mail'); //http://site.com/api/mail/confirm
-    Route::post('confirmed', 'API\AuthController@confirmRegister'); //http://site.com/api/mail/confirmed
-    Route::post('resend', 'API\AuthController@resendConfirmation'); //http://site.com/api/mail/resend
+    Route::post('confirm', 'API\AuthController@register')->name('confirm-mail');
+    //http://site.com/api/mail/confirm
+    Route::post('confirmed', 'API\AuthController@confirmRegister');
+    //http://site.com/api/mail/confirmed
+    Route::post('resend', 'API\AuthController@resendConfirmation');
+    //http://site.com/api/mail/resend
+
     //Rotas para a troca de e-mail (mudar as URIs depois).
-    Route::post('new', 'API\AuthController@requestNewEmail'); //http://site.com/api/mail/new/
-    Route::post('new/confirm', 'API\AuthController@newEmailConfirmed'); //http://site.com/api/mail/new/confirm
+    Route::post('new', 'API\AuthController@requestNewEmail');
+    //http://site.com/api/mail/new/
+    Route::post('new/confirm', 'API\AuthController@newEmailConfirmed');
+    //http://site.com/api/mail/new/confirm
 });
 
 // Grupo de rotas para usuários logados.
@@ -49,7 +63,8 @@ Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'auth'
 ], function (){
-    Route::post('logout', 'API\AuthController@logout'); //http://site.com/api/auth/logout
+    Route::post('logout', 'API\AuthController@logout');
+    //http://site.com/api/auth/logout
 });
 
 // Rotas de log-in pelo FaceBook e pelo Google.
@@ -59,7 +74,8 @@ Route::group([
 ], function (){
     Route::get('/facebook', 'API\FacebookController@redirectToFacebook'); //http://site.com/api/social/login/facebook
     Route::get('/facebook/callback', 'API\FacebookController@handleFacebookCallback'); //http://site.com/api/social/login/facebook/callback
-    Route::get('/google', 'API\GoogleController@redirectToGoogle'); //http://site.com/api/social/login/google
+    Route::get('/google', 'API\GoogleController@redirectToGoogle');
+    //http://site.com/api/social/login/google
     Route::get('/google/callback', 'API\GoogleController@handleGoogleCallback'); //http://site.com/api/social/login/google/callback
 });
 
@@ -68,13 +84,20 @@ Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'users'
 ], function (){
-    Route::get('{id}', 'UserController@show'); //http://site.com/api/users/id
-    Route::get('/', 'UserController@index'); //http://site.com/api/users/
-    Route::post('/', 'UserController@store'); //http://site.com/api/users/
-    Route::put('{id}', 'UserController@update'); //http://site.com/api/users/id
-    Route::delete('{id}', 'UserController@delete'); //http://site.com/api/users/id
-
-    Route::post('inscreve-atividade/{id}', 'UserController@inscreveAtividadePacote'); //http://site.com/api/users/inscreve-atividade/id
+    Route::get('{id}', 'UserController@show');
+    //http://site.com/api/users/id
+    Route::get('/', 'UserController@index');
+    //http://site.com/api/users/
+    Route::post('/', 'UserController@store');
+    //http://site.com/api/users/
+    Route::put('{id}', 'UserController@update');
+    //http://site.com/api/users/id
+    Route::delete('{id}', 'UserController@delete');
+    //http://site.com/api/users/id
+    Route::post('inscreve-atividade/{id}', 'UserController@inscreveAtividadePacote');
+    //http://site.com/api/users/inscreve-atividade/id
+    Route::post('remove-atividade/{id}', 'UserController@desinscreveAtividade');
+    //http://site.com/api/users/desinscreve-atividade/id
 });
 
 Route::group([
@@ -88,11 +111,16 @@ Route::group([
     'middleware' => 'auth:api',
     'prefix' => 'atividades'
 ], function (){
-    Route::get('{id}', 'AtividadesController@show'); //http://site.com/api/atividades/id
-    Route::get('/', 'AtividadesController@index'); //http://site.com/api/atividades/
-    Route::post('/', 'AtividadesController@store'); //http://site.com/api/atividades/
-    Route::put('{id}', 'AtividadesController@update'); //http://site.com/api/atividades/id
-    Route::delete('{id}', 'AtividadesController@delete'); //http://site.com/api/atividades/id
+    Route::get('{id}', 'AtividadesController@show');
+    //http://site.com/api/atividades/id
+    Route::get('/', 'AtividadesController@index');
+    //http://site.com/api/atividades/
+    Route::post('/', 'AtividadesController@store');
+    //http://site.com/api/atividades/
+    Route::put('{id}', 'AtividadesController@update');
+    //http://site.com/api/atividades/id
+    Route::delete('{id}', 'AtividadesController@delete');
+    //http://site.com/api/atividades/id
 });
 
 
@@ -106,21 +134,29 @@ Route::group([//criar middleware para restringir acesso
     //Rotas de pacotes
     Route::group(['prefix' => 'pacotes'], function(){
 
-        Route::get('/','PacoteController@index');                   //http://site.com/api/admin/pacotes/
-        Route::get('{id}','PacoteController@showPacote');           //http://site.com/api/admin/pacotes/id
-        Route::post('/', 'PacoteController@storePacote');           //http://site.com/api/admin/pacotes/
-        Route::put('{id}', 'PacoteController@updatePacote');        //http://site.com/api/admin/pacotes/id
-        Route::delete('{id}', 'PacoteController@destroyPacote');    //http://site.com/api/admin/pacotes/id
+        Route::get('/','PacoteController@index');
+        //http://site.com/api/admin/pacotes/
+        Route::get('{id}','PacoteController@showPacote');
+        //http://site.com/api/admin/pacotes/id
+        Route::post('/', 'PacoteController@storePacote');
+        //http://site.com/api/admin/pacotes/
+        Route::put('{id}', 'PacoteController@updatePacote');
+        //http://site.com/api/admin/pacotes/id
+        Route::delete('{id}', 'PacoteController@destroyPacote');
+        //http://site.com/api/admin/pacotes/id
     });
 
     // rota de edição dos lotes
     Route::group(['prefix' => 'lotes'], function(){
-        Route::put('{id}', 'LoteController@updateLote');            //http://site.com/api/admin/lotes/id
+        Route::put('{id}', 'LoteController@updateLote');
+        //http://site.com/api/admin/lotes/id
     });
 
     //rotas de atualizar configurações
-    Route::put('inscricoes', 'ConfigController@updateInscricoes');      //http://site.com/api/admin/inscricoes
-    Route::put('evento', 'ConfigController@updateEvento');              //http://site.com/api/admin/evento
+    Route::put('inscricoes', 'ConfigController@updateInscricoes');
+    //http://site.com/api/admin/inscricoes
+    Route::put('evento', 'ConfigController@updateEvento');
+    //http://site.com/api/admin/evento
     Route::put('abreinscricoes', 'ConfigController@abreInscricoes');    //http://site.com/api/admin/abreinscricoes
     Route::put('fechainscricoes', 'ConfigController@fechaInscricoes');  //http://site.com/api/admin/fechainscricoes
 
