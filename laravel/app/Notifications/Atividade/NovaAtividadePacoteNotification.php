52c07978e37a163cfa957fc05200c94a75cc59c1
celle <Marcelleflucaraa@yahoo.com.br>
2018-09-27 16:03:49 -0300

    * pequenas alterações no texto da notification

--- laravel/app/Notifications/Atividade/NovaAtividadePacoteNotification.php
+++ laravel/app/Notifications/Atividade/NovaAtividadePacoteNotification.php
@@ -10,34 +10,17 @@ use Illuminate\Notifications\Messages\MailMessage;
 class NovaAtividadePacoteNotification extends Notification
 {
     use Queueable;
-
-    /**
-     * Create a new notification instance.
-     *
-     * @return void
-     */
+    
     public function __construct()
     {
         //
     }
 
-    /**
-     * Get the notification's delivery channels.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function via($notifiable)
     {
         return ['mail'];
     }
 
-    /**
-     * Get the mail representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return \Illuminate\Notifications\Messages\MailMessage
-     */
     public function toMail($notifiable)
     {
         return (new MailMessage)
@@ -48,12 +31,6 @@ class NovaAtividadePacoteNotification extends Notification
         ->line('Atenciosamente, equipe '.env('APP_NAME').'.');
     }
 
-    /**
-     * Get the array representation of the notification.
-     *
-     * @param  mixed  $notifiable
-     * @return array
-     */
     public function toArray($notifiable)
     {
         return [
