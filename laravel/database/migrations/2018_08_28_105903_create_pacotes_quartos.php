e78b64c6df512333c0581ef13cb6021450ea2752
Carrasco <fernandocarrasco@ejcm.com.br>
2018-09-28 14:14:04 -0300

    * desativando as foreign key constraints das migrations para facilitar o drop do bd pelo terminal

--- laravel/database/migrations/2018_08_28_105903_create_pacotes_quartos.php
+++ laravel/database/migrations/2018_08_28_105903_create_pacotes_quartos.php
@@ -39,6 +39,8 @@ class CreatePacotesQuartos extends Migration
      */
     public function down()
     {
+        Schema::disableForeignKeyConstraints();
         Schema::dropIfExists('pacotes_quartos');
+        Schema::enableForeignKeyConstraints();
     }
 }
