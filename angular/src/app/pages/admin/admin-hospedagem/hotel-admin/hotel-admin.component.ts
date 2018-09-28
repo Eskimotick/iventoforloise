fd893158d670ebe891a779d7d46d9fc31fc22194
Loise <lobo.loise@ejcm.com.br>
2018-09-28 14:53:32 -0300

    upload de hotel e foto

--- angular/src/app/pages/admin/admin-hospedagem/hotel-admin/hotel-admin.component.ts
+++ angular/src/app/pages/admin/admin-hospedagem/hotel-admin/hotel-admin.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-hotel-admin',
@@ -6,6 +6,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hotel-admin.component.css']
 })
 export class HotelAdminComponent implements OnInit {
+
+  @Input('hoteis') hoteis: any;
+  @Input('hotelFoto') hotelFoto: string;
 
   constructor() { }
 
