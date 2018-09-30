27fcdb112ee37fb742b7afa1d4f23fe3d0a986e8
Loise <lobo.loise@gmail.com>
2018-09-30 00:57:34 -0300

    * hospedagem admin

--- angular/src/app/pages/admin/admin-hospedagem/hotel-admin/hotel-admin.component.ts
+++ angular/src/app/pages/admin/admin-hospedagem/hotel-admin/hotel-admin.component.ts
@@ -64,7 +64,7 @@ export class HotelAdminComponent implements OnInit {
     this.deleteHotelEmitter.emit(this.hotel.id);
   }
 
-  editHotelEmitter() {
+  openEditModal() {
     this.editHotelEmitter.emit(this.hotel.id);
   }
 
